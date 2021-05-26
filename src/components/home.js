8265e31f2caa0be8d44af073e4c1d92b89bc2677
MatthiasvsGitHub <matthias@schmettow.de>
2021-05-26 16:14:11 +0200

    went crazy with the pokefont

--- src/components/home.js
+++ src/components/home.js
@@ -9,6 +9,7 @@ import { Label, Select } from '@rebass/forms'
 import { Box, Card, Image, Heading, Text, Flex } from 'rebass'
 import './home.css'
 import './startScreen.css'
+import './font.css'
 import Modal from 'react-modal';
 
 const customStyles = {
@@ -121,32 +122,33 @@ export default function Home() {
                 <Box width={[1/2, 4 / 11]} px={2}>
                     <Text p={1} color='background' bg='primary'>
                         <br />
-                        {img && pokeData.name ? <><h3>{pokeData.name.english}</h3> <img src={img} className='pokeImg flip-horizontally' a={pokeData.name.english} /> </>
+                        {img && pokeData.name ? <><h1 className='pokefont'>{pokeData.name.english}</h1> <img src={img} className='pokeImg flip-horizontally' a={pokeData.name.english} /> </>
                         : <img src={pokeball} className='pokeImg' a='pokeball' />}
                     </Text>
                 </Box>
                 <Box width={[1/2, 3 / 11]} px={2}>
                     <Text p={1} color='background' bg='primary'>
-                        <h2 className='selectHeading'>Please Select your own Pokemon</h2>
+                        <h2 className='selectHeading pokefont'>Please Select your own Pokemon</h2>
                         {data ?
                             <>
                                 <Box>
-                                    <Label htmlFor='pokemon'>Choose your fighter!</Label>
+                                    <Label htmlFor='pokemon' className='pokefont'>Choose your fighter!</Label>
                                     <Select
+                                        className='pokefont'
                                         id='pokemon'
                                         name='pokemon'
                                         defaultValue={null}
                                         onChange={((e) => setSelectedPokemon(e.target.selectedIndex + 1))}>
                                         {Object.entries(data).map(([key, e]) => (
-                                            <option
+                                            <option className='pokefont'
                                                 key={key}>
                                                 {e.name.english}
                                             </option>
                                         ))}
                                     </Select>
                                 </Box>
-                                <button className='selectButton' onClick={(() => setSelectedPokemon(Math.ceil(Math.random() * 808)))}>Random Pokemon</button> <br/>
-                                {img && pokeData.name && <button className='selectButton' onClick={selectFirstPokemon}>Select {pokeData.name.english} as your Pokemon!</button>}
+                                <button className='selectButton pokefont' onClick={(() => setSelectedPokemon(Math.ceil(Math.random() * 808)))}>Random Pokemon</button> <br/>
+                                {img && pokeData.name && <button className='selectButton pokefont' onClick={selectFirstPokemon}>Select {pokeData.name.english} as your Pokemon!</button>}
                             </>
                             : 'loading...'}
                     </Text>
@@ -154,17 +156,17 @@ export default function Home() {
                 <Box width={[1/1, 4 / 11]} px={2}>
                     <Text p={1} color='background' bg='primary'>
                         {pokeData.name ? <>
-                            <h3>Stats of {pokeData.name.english}:</h3>
-                            <h4>Type: {pokeData.type[0]}{pokeData.type[1] && `, ${pokeData.type[1]}`}</h4>
+                            <h3 className='pokefont'>Stats of {pokeData.name.english}:</h3>
+                            <h4 className='pokefont'>Type: {pokeData.type[0]}{pokeData.type[1] && `, ${pokeData.type[1]}`}</h4>
                             <ul>
-                                <li>HP: {pokeData.base.HP}</li>
-                                <li>Attack: {pokeData.base.Attack}</li>
-                                <li>Defense: {pokeData.base.Defense}</li>
-                                <li>SP. Attack: {pokeData.base['Sp. Attack']}</li>
-                                <li>SP. Defense: {pokeData.base['Sp. Defense']}</li>
-                                <li>Speed: {pokeData.base.Speed}</li>
+                                <li className='pokefont'>HP: {pokeData.base.HP}</li>
+                                <li className='pokefont'>Attack: {pokeData.base.Attack}</li>
+                                <li className='pokefont'>Defense: {pokeData.base.Defense}</li>
+                                <li className='pokefont'>SP. Attack: {pokeData.base['Sp. Attack']}</li>
+                                <li className='pokefont'>SP. Defense: {pokeData.base['Sp. Defense']}</li>
+                                <li className='pokefont'>Speed: {pokeData.base.Speed}</li>
                             </ul>
-                        </> : <><p>Select a Pokemon to show its Stats :)</p></>}
+                        </> : <><p className='pokefont'>Select a Pokemon to show its Stats</p></>}
                     </Text>
                 </Box>
             </Flex>
@@ -177,41 +179,42 @@ export default function Home() {
                     <Box width={4 / 11} px={2}>
                     <Text p={1} color='background' bg='primary'>
                         {pokeData.name ? <>
-                            <h3>Stats of {pokeData.name.english}:</h3>
-                                <h4>Type: {pokeData.type[0]}{pokeData.type[1] && `, ${pokeData.type[1]}`}</h4>
+                            <h3 className='pokefont'>Stats of {pokeData.name.english}:</h3>
+                                <h4 className='pokefont'>Type: {pokeData.type[0]}{pokeData.type[1] && `, ${pokeData.type[1]}`}</h4>
                                 <ul>
-                                    <li>HP: {pokeData.base.HP}</li>
-                                    <li>Attack: {pokeData.base.Attack}</li>
-                                    <li>Defense: {pokeData.base.Defense}</li>
-                                    <li>SP. Attack: {pokeData.base['Sp. Attack']}</li>
-                                    <li>SP. Defense: {pokeData.base['Sp. Defense']}</li>
-                                    <li>Speed: {pokeData.base.Speed}</li>
+                                    <li className='pokefont'>HP: {pokeData.base.HP}</li>
+                                    <li className='pokefont'>Attack: {pokeData.base.Attack}</li>
+                                    <li className='pokefont'>Defense: {pokeData.base.Defense}</li>
+                                    <li className='pokefont'>SP. Attack: {pokeData.base['Sp. Attack']}</li>
+                                    <li className='pokefont'>SP. Defense: {pokeData.base['Sp. Defense']}</li>
+                                    <li className='pokefont'>Speed: {pokeData.base.Speed}</li>
                                 </ul>
-                            </> : <><p>Select a Pokemon to show its Stats :)</p></>}
+                            </> : <><p className='pokefont'>Select a Pokemon to show its Stats</p></>}
                         </Text>
                     </Box>
                     <Box width={3 / 11} px={2}>
                         <Text p={1} color='background' bg='primary'>
-                        <h2 className='selectHeading'>Please Select your opponents Pokemon</h2>
+                        <h2 className='selectHeading pokefont'>Please Select your opponents Pokemon</h2>
                         {data ?
                                 <>
                                     <Box>
-                                        <Label htmlFor='pokemon'>Choose who you fight against!</Label>
+                                        <Label className='pokefont' htmlFor='pokemon'>Choose who you fight against!</Label>
                                         <Select
+                                            className='pokefont'
                                             id='pokemon'
                                             name='pokemon'
                                             defaultValue={null}
                                             onChange={((e) => setSelectedPokemon(e.target.selectedIndex + 1))}>
                                             {Object.entries(data).map(([key, e]) => (
-                                                <option
+                                                <option className='pokefont'
                                                     key={key}>
                                                     {e.name.english}
                                                 </option>
                                             ))}
                                         </Select>
                                     </Box>
-                                    <button className='selectButton' onClick={(() => setSelectedPokemon(Math.ceil(Math.random() * 808)))}>Random Pokemon</button> <br/>
-                                    {img && pokeData.name && <button className='selectButton' onClick={selectSecondPokemon}>Select {pokeData.name.english} as your opponents Pokemon!</button>}
+                                    <button className='selectButton pokefont' onClick={(() => setSelectedPokemon(Math.ceil(Math.random() * 808)))}>Random Pokemon</button> <br/>
+                                    {img && pokeData.name && <button className='selectButton pokefont' onClick={selectSecondPokemon}>Select {pokeData.name.english} as your opponents Pokemon!</button>}
                                 </>
                                 : 'loading...'}
                         </Text>
@@ -219,7 +222,7 @@ export default function Home() {
                     <Box width={4 / 11} px={2}>
                     <Text p={1} color='background' bg='primary'>
                         <br />
-                            {img && pokeData.name ? <><h3>{pokeData.name.english}</h3> <img src={img} className='pokeImg' a={pokeData.name.english} /> </>
+                            {img && pokeData.name ? <><h1 className='pokefont'>{pokeData.name.english}</h1> <img src={img} className='pokeImg' a={pokeData.name.english} /> </>
                             : <img src={pokeball} className='pokeImg' a='pokeball' />}
                         </Text>
                     </Box>
