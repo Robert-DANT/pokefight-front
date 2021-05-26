b41c9ee7e8110ca10fa69007e37ec70f20a28dff
MatthiasvsGitHub <matthias@schmettow.de>
2021-05-26 18:35:39 +0200

    major cleanup - seperate components and more

--- src/components/home.js
+++ src/components/home.js
@@ -1,16 +1,12 @@
 import Axios from 'axios'
 import { useEffect, useState } from 'react'
-import chooseOwnImg from './img/chooseOwn.png'
-import chooseOppImg from './img/chooseOpp.png'
-import ball from './img/ball.png'
-import pokeball from './img/pokeball.png'
-import video from './img/video.mp4'
-import { Label, Select } from '@rebass/forms'
-import { Box, Card, Image, Heading, Text, Flex } from 'rebass'
 import './home.css'
 import './startScreen.css'
 import './font.css'
 import Modal from 'react-modal';
+import StartScreen from './startScreen'
+import OwnSelectionScreen from './ownSelectionScreen'
+import OpponentSelectionScreen from './opponentSelectionScreen'
 
 const customStyles = {
   content : {
@@ -119,150 +115,12 @@ export default function Home() {
           style={customStyles}
           contentLabel="Start Screen Modal"
         >
-            {/* Outer Wrapper */}
-      <section className="videoBg" onClick={(() => setStartScreen(false))}>
-        {/* Video */}
-        <div className="videoPlace">
-          <video src={video} autoPlay muted loop className="VideoSection" />
-        </div>
-        {/* Overlay */}
-        <div className="VideoOverlay" />
-        {/* Content */}
-        <div className="VideoContent">
-          <div className="Container">
-            <img src="https://fontmeme.com/permalink/210526/d6e962a9abeca391ff743d18f8288629.png" alt="pokemon-font" border={0} />
-            <div className="start">
-              <div>
-                <img className="ball" src={ball} />
-              </div>
-              <div>
-                <h1>Click to start</h1>
-              </div>
-              <div>
-                <img className="ball" src={ball} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+            <StartScreen setStartScreen={setStartScreen}/>
         </Modal>
-            {!ownPokemon ?
-            <div className='selectionScreen'>
-            <img src={chooseOwnImg} className='headingText '/>
-            <Flex mx={-2} className='ownSelectBox'>
-                <Box width={[1/2, 4 / 11]} px={2}>
-                    <Text p={1} color='background' bg='primary'>
-                        <br />
-                        {img && pokeData.name ? <><h1 className='pokefont'>{pokeData.name.english}</h1> <img src={img} className='pokeImg flip-horizontally' a={pokeData.name.english} /> </>
-                        : <img src={pokeball} className='pokeImg' a='pokeball' />}
-                    </Text>
-                </Box>
-                <Box width={[1/2, 3 / 11]} px={2}>
-                    <Text p={1} color='background' bg='primary'>
-                        <h2 className='selectHeading pokefont'>Please Select your own Pokemon</h2>
-                        {data ?
-                            <>
-                                <Box>
-                                    <Label htmlFor='pokemon' className='pokefont'>Choose your fighter!</Label>
-                                    <Select
-                                        className='pokefont'
-                                        id='pokemon'
-                                        name='pokemon'
-                                        defaultValue={null}
-                                        onChange={((e) => setSelectedPokemon(e.target.selectedIndex + 1))}>
-                                        {Object.entries(data).map(([key, e]) => (
-                                            <option className='pokefont'
-                                                key={key}>
-                                                {e.name.english}
-                                            </option>
-                                        ))}
-                                    </Select>
-                                </Box>
-                                <button className='selectButton pokefont' onClick={(() => setSelectedPokemon(Math.ceil(Math.random() * 808)))}>Random Pokemon</button> <br/>
-                                {img && pokeData.name && <button className='selectButton pokefont' onClick={selectFirstPokemon}>Select {pokeData.name.english} as your Pokemon!</button>}
-                            </>
-                            : 'loading...'}
-                    </Text>
-                </Box>
-                <Box width={[1/1, 4 / 11]} px={2}>
-                    <Text p={1} color='background' bg='primary'>
-                        {pokeData.name ? <>
-                            <h3 className='pokefont selectHeading'>Stats of {pokeData.name.english}:</h3>
-                            <div className='statsList'>
-                            <ul>
-                                <li className='pokefont'>Type: {pokeData.type[0]}{pokeData.type[1] && `, ${pokeData.type[1]}`}</li>
-                                <li className='pokefont'>HP: {pokeData.base.HP}</li>
-                                <li className='pokefont'>Attack: {pokeData.base.Attack}</li>
-                                <li className='pokefont'>Defense: {pokeData.base.Defense}</li>
-                                <li className='pokefont'>SP. Attack: {pokeData.base['Sp. Attack']}</li>
-                                <li className='pokefont'>SP. Defense: {pokeData.base['Sp. Defense']}</li>
-                                <li className='pokefont'>Speed: {pokeData.base.Speed}</li>
-                                
-                            </ul></div>
-                        </> : <><p className='pokefont selectHeading'>Select a Pokemon to show its Stats</p></>}
-                    </Text>
-                </Box>
-            </Flex>
-            </div>: !opponentPokemon ?
-
-                /* Here Starts the Opponent Select */
-                <div className='selectionScreen' style={{background: 'linear-gradient(90deg, rgba(101,4,186,1) 0%, rgba(237,123,129,1) 100%)'}}>
-                <img src={chooseOppImg} className='headingText '/>
-                <Flex mx={-2} className='oppSelectBox'>
-                    <Box width={4 / 11} px={2}>
-                    <Text p={1} color='background' bg='primary'>
-                        {pokeData.name ? <>
-                            <h3 className='pokefont selectHeading'>Stats of {pokeData.name.english}:</h3>
-                            <div className='statsList'>
-                                <ul>
-                                    <li className='pokefont'>Type: {pokeData.type[0]}{pokeData.type[1] && `, ${pokeData.type[1]}`}</li>
-                                    <li className='pokefont'>HP: {pokeData.base.HP}</li>
-                                    <li className='pokefont'>Attack: {pokeData.base.Attack}</li>
-                                    <li className='pokefont'>Defense: {pokeData.base.Defense}</li>
-                                    <li className='pokefont'>SP. Attack: {pokeData.base['Sp. Attack']}</li>
-                                    <li className='pokefont'>SP. Defense: {pokeData.base['Sp. Defense']}</li>
-                                    <li className='pokefont'>Speed: {pokeData.base.Speed}</li>
-                                </ul>
-                                </div>
-                            </> : <><p className='pokefont selectHeading'>Select a Pokemon to show its Stats</p></>}
-                        </Text>
-                    </Box>
-                    <Box width={3 / 11} px={2}>
-                        <Text p={1} color='background' bg='primary'>
-                        <h2 className='selectHeading pokefont'>Please Select your opponents Pokemon</h2>
-                        {data ?
-                                <>
-                                    <Box>
-                                        <Label className='pokefont' htmlFor='pokemon'>Choose who you fight against!</Label>
-                                        <Select
-                                            className='pokefont'
-                                            id='pokemon'
-                                            name='pokemon'
-                                            defaultValue={null}
-                                            onChange={((e) => setSelectedPokemon(e.target.selectedIndex + 1))}>
-                                            {Object.entries(data).map(([key, e]) => (
-                                                <option className='pokefont'
-                                                    key={key}>
-                                                    {e.name.english}
-                                                </option>
-                                            ))}
-                                        </Select>
-                                    </Box>
-                                    <button className='selectButton pokefont' onClick={(() => setSelectedPokemon(Math.ceil(Math.random() * 808)))}>Random Pokemon</button> <br/>
-                                    {img && pokeData.name && <button className='selectButton pokefont' onClick={selectSecondPokemon}>Select {pokeData.name.english} as your opponents Pokemon!</button>}
-                                </>
-                                : 'loading...'}
-                        </Text>
-                    </Box>
-                    <Box width={4 / 11} px={2}>
-                    <Text p={1} color='background' bg='primary'>
-                        <br />
-                            {img && pokeData.name ? <><h1 className='pokefont'>{pokeData.name.english}</h1> <img src={img} className='pokeImg' a={pokeData.name.english} /> </>
-                            : <img src={pokeball} className='pokeImg' a='pokeball' />}
-                        </Text>
-                    </Box>
-                </Flex>
-                </div> : /* Here comes the winner screen */
+            {!ownPokemon ? <OwnSelectionScreen pokeData={pokeData} img={img} data={data} selectFirstPokemon={selectFirstPokemon} setSelectedPokemon={setSelectedPokemon}/>
+            : !opponentPokemon ?
+                <OpponentSelectionScreen pokeData={pokeData} img={img} data={data} selectSecondPokemon={selectSecondPokemon} setSelectedPokemon={setSelectedPokemon} /> :
+                /* Here comes the winner screen */
                 <>
                 <h1>The winner is: {winner}</h1>
                 <button onClick={startFromBeginning} >Start Again :)</button>
---
+++ src/components/startScreen.js
@@ -0,0 +1,33 @@
+import ball from './img/ball.png'
+import video from './img/video.mp4'
+import './startScreen.css'
+
+export default function StartScreen ({setStartScreen}) {
+    return(
+      <section className="videoBg" onClick={(() => setStartScreen(false))}>
+      {/* Video */}
+      <div className="videoPlace">
+        <video src={video} autoPlay muted loop className="VideoSection" />
+      </div>
+      {/* Overlay */}
+      <div className="VideoOverlay" />
+      {/* Content */}
+      <div className="VideoContent">
+        <div className="Container">
+          <img src="https://fontmeme.com/permalink/210526/d6e962a9abeca391ff743d18f8288629.png" alt="pokemon-font" border={0} />
+          <div className="start">
+            <div>
+              <img className="ball" src={ball} />
+            </div>
+            <div>
+              <h1>Click to start</h1>
+            </div>
+            <div>
+              <img className="ball" src={ball} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    )
+}
