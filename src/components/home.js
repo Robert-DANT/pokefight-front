6c55bda8a9b6ef2f2fe7017491495e6329da7e39
MatthiasvsGitHub <matthias@schmettow.de>
2021-05-26 15:55:40 +0200

    added starting overlay

--- src/components/home.js
+++ src/components/home.js
@@ -2,10 +2,25 @@ import Axios from 'axios'
 import { useEffect, useState } from 'react'
 import chooseOwnImg from './img/chooseOwn.png'
 import chooseOppImg from './img/chooseOpp.png'
+import ball from './img/ball.png'
 import pokeball from './img/pokeball.png'
+import video from './img/video.mp4'
 import { Label, Select } from '@rebass/forms'
 import { Box, Card, Image, Heading, Text, Flex } from 'rebass'
 import './home.css'
+import './startScreen.css'
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 export default function Home() {
     const [data, setData] = useState()
@@ -14,6 +29,7 @@ export default function Home() {
     const [img, setImg] = useState()
     const [ownPokemon, setOwnPokemon] = useState()
     const [opponentPokemon, setOpponentPokemon] = useState()
+    const [startScreen, setStartScreen] = useState(true)
 
     useEffect(() => {
         fetchData();
@@ -48,7 +64,6 @@ export default function Home() {
         setImg()
         setPokeData('')
         setSelectedPokemon('')
-        document.body.style.backgroundColor = 'lightblue';
     }
 
     const selectSecondPokemon = () => {
@@ -56,6 +71,7 @@ export default function Home() {
         setImg()
         setPokeData('')
         setSelectedPokemon('')
+        setStartScreen(true)
     }
 
 
@@ -63,18 +79,53 @@ export default function Home() {
 
     return (
         <>
-            
+        <Modal
+         closeTimeoutMS={200}
+          isOpen={startScreen}
+          /* onAfterOpen={} */
+          onRequestClose={(() => setStartScreen(false))}
+          style={customStyles}
+          contentLabel="Start Screen Modal"
+        >
+            {/* Outer Wrapper */}
+      <section className="videoBg" onClick={(() => setStartScreen(false))}>
+        {/* Video */}
+        <div className="videoPlace">
+          <video src={video} autoPlay muted loop className="VideoSection" />
+        </div>
+        {/* Overlay */}
+        <div className="VideoOverlay" />
+        {/* Content */}
+        <div className="VideoContent">
+          <div className="Container">
+            <img src="https://fontmeme.com/permalink/210526/d6e962a9abeca391ff743d18f8288629.png" alt="pokemon-font" border={0} />
+            <div className="start">
+              <div>
+                <img className="ball" src={ball} />
+              </div>
+              <div>
+                <h1>Click to start</h1>
+              </div>
+              <div>
+                <img className="ball" src={ball} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+        </Modal>
             {!ownPokemon ?
-            <>
+            <div className='selectionScreen'>
             <img src={chooseOwnImg} className='headingText '/>
             <Flex mx={-2} className='ownSelectBox'>
-                <Box width={1 / 3} px={2}>
+                <Box width={[1/2, 4 / 11]} px={2}>
                     <Text p={1} color='background' bg='primary'>
                         <br />
-                        {img && pokeData.name ? <img src={img} className='pokeImg flip-horizontally' a={pokeData.name.english} /> : <img src={pokeball} className='pokeImg' a='pokeball' />}
+                        {img && pokeData.name ? <><h3>{pokeData.name.english}</h3> <img src={img} className='pokeImg flip-horizontally' a={pokeData.name.english} /> </>
+                        : <img src={pokeball} className='pokeImg' a='pokeball' />}
                     </Text>
                 </Box>
-                <Box width={1 / 3} px={2}>
+                <Box width={[1/2, 3 / 11]} px={2}>
                     <Text p={1} color='background' bg='primary'>
                         <h2 className='selectHeading'>Please Select your own Pokemon</h2>
                         {data ?
@@ -94,16 +145,16 @@ export default function Home() {
                                         ))}
                                     </Select>
                                 </Box>
-
+                                <button className='selectButton' onClick={(() => setSelectedPokemon(Math.ceil(Math.random() * 808)))}>Random Pokemon</button> <br/>
                                 {img && pokeData.name && <button className='selectButton' onClick={selectFirstPokemon}>Select {pokeData.name.english} as your Pokemon!</button>}
                             </>
                             : 'loading...'}
                     </Text>
                 </Box>
-                <Box width={1 / 3} px={2}>
+                <Box width={[1/1, 4 / 11]} px={2}>
                     <Text p={1} color='background' bg='primary'>
                         {pokeData.name ? <>
-                            <h3>{pokeData.name.english}</h3>
+                            <h3>Stats of {pokeData.name.english}:</h3>
                             <h4>Type: {pokeData.type[0]}{pokeData.type[1] && `, ${pokeData.type[1]}`}</h4>
                             <ul>
                                 <li>HP: {pokeData.base.HP}</li>
@@ -117,16 +168,16 @@ export default function Home() {
                     </Text>
                 </Box>
             </Flex>
-            </>:
+            </div>:
 
                 /* Here Starts the Opponent Select */
-                <>
+                <div className='selectionScreen' style={{background: 'linear-gradient(90deg, rgba(101,4,186,1) 0%, rgba(237,123,129,1) 100%)'}}>
                 <img src={chooseOppImg} className='headingText '/>
                 <Flex mx={-2} className='oppSelectBox'>
-                    <Box width={1 / 3} px={2}>
+                    <Box width={4 / 11} px={2}>
                     <Text p={1} color='background' bg='primary'>
                         {pokeData.name ? <>
-                                <h3>{pokeData.name.english}</h3>
+                            <h3>Stats of {pokeData.name.english}:</h3>
                                 <h4>Type: {pokeData.type[0]}{pokeData.type[1] && `, ${pokeData.type[1]}`}</h4>
                                 <ul>
                                     <li>HP: {pokeData.base.HP}</li>
@@ -139,7 +190,7 @@ export default function Home() {
                             </> : <><p>Select a Pokemon to show its Stats :)</p></>}
                         </Text>
                     </Box>
-                    <Box width={1 / 3} px={2}>
+                    <Box width={3 / 11} px={2}>
                         <Text p={1} color='background' bg='primary'>
                         <h2 className='selectHeading'>Please Select your opponents Pokemon</h2>
                         {data ?
@@ -159,20 +210,21 @@ export default function Home() {
                                             ))}
                                         </Select>
                                     </Box>
-
+                                    <button className='selectButton' onClick={(() => setSelectedPokemon(Math.ceil(Math.random() * 808)))}>Random Pokemon</button> <br/>
                                     {img && pokeData.name && <button className='selectButton' onClick={selectSecondPokemon}>Select {pokeData.name.english} as your opponents Pokemon!</button>}
                                 </>
                                 : 'loading...'}
                         </Text>
                     </Box>
-                    <Box width={1 / 3} px={2}>
+                    <Box width={4 / 11} px={2}>
                     <Text p={1} color='background' bg='primary'>
                         <br />
-                            {img && pokeData.name ? <img src={img} className='pokeImg' a={pokeData.name.english} /> : <img src={pokeball} className='pokeImg' a='pokeball' />}
+                            {img && pokeData.name ? <><h3>{pokeData.name.english}</h3> <img src={img} className='pokeImg' a={pokeData.name.english} /> </>
+                            : <img src={pokeball} className='pokeImg' a='pokeball' />}
                         </Text>
                     </Box>
                 </Flex>
-                </>}
+                </div>}
         </>
     )
 }
