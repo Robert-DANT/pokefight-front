ede8e49099968e0c76b5c7c8605a632fb47e1e1f
MatthiasvsGitHub <matthias@schmettow.de>
2021-05-28 16:12:45 +0200

    simple leaderboard integration

--- src/components/winner.js
+++ src/components/winner.js
@@ -4,17 +4,25 @@ import { useEffect, useState } from 'react'
 import Axios from 'axios'
 /* import './font.css' */
 
-export default function Winner ({name, winner, startFromBeginning}) {
+export default function Winner ({name, winner, startFromBeginning, opponentPokemonPower, ownPokemonPower}) {
   const [img, setImg] = useState()
+  const [leaderboard, setLeaderboard] = useState()
 
   useEffect(() => {
     fetchImg()
+    fetchLeadb()
     console.log(winner[1])
 }, []);
 
 const fetchImg = async () => {
   await Axios.get(`https://pokeapi.co/api/v2/pokemon/${winner[1]}`)
       .then((response) => setImg(response.data.sprites.front_default))
+      .catch((error) => console.log(error));
+};
+
+const fetchLeadb = async () => {
+  await Axios.get(`https://pokefight-leaderboard.herokuapp.com/leaderboard`)
+      .then((response) => setLeaderboard(response.data))
       .catch((error) => console.log(error));
 };
 
@@ -29,8 +37,21 @@ const fetchImg = async () => {
             {winner[0] == 'You' ? <p className="pokefontWinner">Your {name} Wins!</p> :
             <p className="pokefontWinner"> Your Opponent's {name} Wins!</p>}
           </div>
+          <div className='leaderboard pokefont'>
+            Leaderboard: <br/>
+            {leaderboard && <>1. {leaderboard[0].name} {leaderboard[0].score} <br/>
+            2. {leaderboard[1].name} {leaderboard[1].score} <br/>
+            3. {leaderboard[2].name} {leaderboard[2].score} <br/>
+            {leaderboard[4] && <>4. {leaderboard[3].name} {leaderboard[3].score} <br/>
+            5. {leaderboard[4].name} {leaderboard[4].score} <br/> </>}
+            </>}
+          </div>
           <div>
-         <img src={img} width="500px" height="500px"  />
+         <img src={img} width="400px" height="400px"  />
+          </div>
+          <div className='saveScore pokefont'>
+            Your Score is {ownPokemonPower-opponentPokemonPower}! <br/>
+            
           </div>
           <div>
           <button className="pokefontStart startAgainButton" onClick={startFromBeginning} >Start Over</button>
